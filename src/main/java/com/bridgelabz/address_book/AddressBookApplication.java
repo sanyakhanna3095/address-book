378a4e1982f99d58ad8f2801801cbe2373093b58
sanyakhanna3095 <sanya1266.be21@chitkara.edu.in>
2025-04-21 13:39:09 +0530

    [Sanya] Add: Logging and Database setup

--- src/main/java/com/bridgelabz/address_book/AddressBookApplication.java
+++ src/main/java/com/bridgelabz/address_book/AddressBookApplication.java
@@ -1,13 +1,17 @@
 package com.bridgelabz.address_book;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
+@Slf4j
 public class AddressBookApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AddressBookApplication.class, args);
+		ApplicationContext context= SpringApplication.run(AddressBookApplication.class, args);
+		log.info("Address Book App Started in {} Environment", context.getEnvironment().getProperty("environment"));
 	}
 
 }
